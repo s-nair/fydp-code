be7a8b953369c1ff0c03ed3a75cb4f22b9fae8ee
Sharath Nair <sharathnair@Sharaths-MacBook-Pro.local>
2016-10-30 00:33:14 -0700

    integrated with eclipse and tried blinky example again

---
+++ slave/beacon/nRF5_SDK_12.1.0_0d23e2a/examples/peripheral/blinky/pca10040/s132/armgcc/_build/nrf52832_xxaa_main.c.d
@@ -0,0 +1,75 @@
+_build/nrf52832_xxaa_main.c.o: ../../../main.c \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../components/drivers_nrf/delay/nrf_delay.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../bsp/bsp.h ../../../../../bsp/boards.h \
+ ../../../../../../components/drivers_nrf/hal/nrf_gpio.h \
+ ../../../../../bsp/pca10040.h
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../components/drivers_nrf/delay/nrf_delay.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../bsp/bsp.h:
+
+../../../../../bsp/boards.h:
+
+../../../../../../components/drivers_nrf/hal/nrf_gpio.h:
+
+../../../../../bsp/pca10040.h:
---
+++ slave/beacon/nRF5_SDK_12.1.0_0d23e2a/examples/peripheral/blinky/pca10040/s132/armgcc/_build/nrf52832_xxaa_nrf_drv_clock.c.d
@@ -0,0 +1,230 @@
+_build/nrf52832_xxaa_nrf_drv_clock.c.o: \
+ ../../../../../../components/drivers_nrf/clock/nrf_drv_clock.c \
+ ../config/sdk_config.h \
+ ../../../../../../components/drivers_nrf/clock/nrf_drv_clock.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdio.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdarg.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../../../../../../components/drivers_nrf/hal/nrf_clock.h \
+ ../../../../../../components/drivers_nrf/common/nrf_drv_common.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_util_platform.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_nvic.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/softdevice/common/softdevice_handler/softdevice_handler.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdlib.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_sdm.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_sdm.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ ../../../../../../components/softdevice/common/softdevice_handler/ble_stack_handler_types.h \
+ ../../../../../../components/softdevice/s132/headers/ble.h \
+ ../../../../../../components/softdevice/s132/headers/ble_ranges.h \
+ ../../../../../../components/softdevice/s132/headers/ble_types.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_l2cap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_err.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gattc.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatts.h \
+ ../../../../../../components/softdevice/common/softdevice_handler/ant_stack_handler_types.h \
+ ../../../../../../components/softdevice/common/softdevice_handler/app_ram_base.h
+
+../config/sdk_config.h:
+
+../../../../../../components/drivers_nrf/clock/nrf_drv_clock.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdio.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/newlib.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/config.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdarg.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/stdio.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
+
+../../../../../../components/drivers_nrf/hal/nrf_clock.h:
+
+../../../../../../components/drivers_nrf/common/nrf_drv_common.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_util_platform.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_nvic.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/softdevice/common/softdevice_handler/softdevice_handler.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdlib.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_sdm.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error_sdm.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+../../../../../../components/softdevice/common/softdevice_handler/ble_stack_handler_types.h:
+
+../../../../../../components/softdevice/s132/headers/ble.h:
+
+../../../../../../components/softdevice/s132/headers/ble_ranges.h:
+
+../../../../../../components/softdevice/s132/headers/ble_types.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_l2cap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_err.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatt.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gattc.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatts.h:
+
+../../../../../../components/softdevice/common/softdevice_handler/ant_stack_handler_types.h:
+
+../../../../../../components/softdevice/common/softdevice_handler/app_ram_base.h:
